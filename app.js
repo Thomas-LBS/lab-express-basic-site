464721fd40d6b2f3846d162125a47ac4991a0010
Thomas Lebas <thomas.lebas26@gmail.com>
2023-08-12 12:01:56 +0200

    done

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express")
+const app = express()
+const port = 3000
+
+app.use(express.static('public'))
+
+app.get("/", (req, res) => {
+
+  res.send(`Thomas's App
+  <a href="home">Go Home Kitty</a>`)
+})
+
+app.get("/home", (req, res) => {
+
+  res.sendFile(__dirname + "/views/home.html")
+  
+})
+
+app.get("/about", (req, res) => {
+  
+  res.sendFile(__dirname + "/views/about.html")
+
+})
+
+app.get("/work", (req, res) => {
+  
+    res.sendFile(__dirname + "/views/work.html")
+  
+  })
+
+app.get("*", (req, res) => {
+
+  res.send("Page not found - 404")
+
+})
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`)
+})
